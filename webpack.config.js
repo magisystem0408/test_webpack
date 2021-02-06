ceb9e066c5439ecebb55cea391721dd24b6a4be1
magisystem0408 <masato.matsudo@gmail.com>
2021-02-06 15:34:16 +0900

    色々また追加した

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} =require('clean-webpack-plugin');
 
+
 module.exports = {
     entry:'./src/javascripts/main.js',
     output: {
@@ -11,18 +12,29 @@ module.exports = {
     },
     module: {
         rules:[
-            {
-            test: /\.css/,
-            use:[
+    {
+        test: /\.css/,
+        use:[
             {
                 loader: MiniCssExtractPlugin.loader,
             },
             {
                     loader:'css-loader',
+            },
+        ],
+    },
+    {
+        test: /\.(png|jpg)/,
+        use:[{
+            loader: 'file-loader',
+            options: {
+                esModule:false,
+                name :'images/[name].[ext]',
             }
-            ]
-            }
-        ]
+        },
+        ],
+    },
+    ],
     },
 
     plugins: [
